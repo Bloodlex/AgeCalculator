85e96adf7c83db71d5cfa069e2c5554c162db444
Bloodlex <bigatex@gmail.com>
2025-02-13 21:39:57 +0100

    initial commit

--- app/src/main/java/com/example/kalkulatorwieku/MainActivity.kt
+++ app/src/main/java/com/example/kalkulatorwieku/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        enableEdgeToEdge()
 
         selectedDateTextView = findViewById(R.id.selectedDateTextView)
         selectDateButton = findViewById(R.id.button)
