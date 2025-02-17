7ba989f2c0e483829d7cc4073976e5ee43a7b751
Bloodlex <bigatex@gmail.com>
2025-02-17 16:28:41 +0100

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,6 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.material)
     testImplementation(libs.junit)
-    testImplementation(libs.mockito.core)
-    testImplementation(libs.mockito.kotlin)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
